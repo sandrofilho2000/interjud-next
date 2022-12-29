4bc42c5511a82ef306119a49152ca6fa2a017f3a
Sandro <sandro.oliveira@rocky.ag>
2022-12-29 02:52:41 -0300

    fourth commit

--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,15 @@ import Vantagens from '../components/Home/Vantagens'
 import Navbar from '../components/Navbar'
 
 export default function Home() {
-  useEffect(() => {
-
-  })
   return (
     <div>
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <html lang="pt-br" />
         <title>InterJud | Plataforma de negociação de crédito judicial</title>
         <link rel="shortcut icon" type="image-x/png" href="https://www.interjud.com.br/icon.png" />
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width"/>
         <meta name="description" content="Compre ou venda créditos judiciais com a segurança da InterJud, Garanta agora seu direito e antecipe seus sonhos, ou Receba um valor justo pelo seu crédito judicial." />
         <meta name="author" content="Design por DamixCode/Desenvolvimento por Aurora Web Design" />

a6b32e4a329b5f42635f0c841f81be4a653bd2d7
Sandro <sandro.oliveira@rocky.ag>
2022-12-30 15:20:40 -0300

    fix: sets elements image right 4

--- components/Credit/index.js
+++ components/Credit/index.js
@@ -54,7 +54,7 @@ const Credit = ({ credit }) => {
         >
             <article className='credit_single'>
                 {credit.img &&
-                    <img width={240} height={330} src={`../public/assets/images/Credits/${credit.img}`} loading="lazy" alt="Banco do brasil" />
+                    <img width={240} height={330} src={`/_next/static/media/${credit.img}`} loading="lazy" alt="Banco do brasil" />
                 }
                 <div className='credit_content'>
                     <div className='rating'>

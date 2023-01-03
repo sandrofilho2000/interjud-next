e390840373bac66f0555f34a61421a6ac6836318
Sandro <sandro.oliveira@rocky.ag>
2023-01-02 21:20:54 -0300

    fix: set quotes images right

--- components/Home/Depoimentos/index.js
+++ components/Home/Depoimentos/index.js
@@ -4,6 +4,7 @@ import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa'
 import LineTitle from '../../LineTitle'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination } from 'swiper';
+import img from '../../../public/assets/images/Depoimentos/foto1.jpg'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -12,38 +13,6 @@ SwiperCore.use([Navigation, Pagination]);
 
 const Depoimentos = () => {
 
-    const quotes = [
-        {
-            id: 0,
-            author: "Daniel",
-            quote: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-            img: "daniel.webp"
-        }
-        ,
-        {
-            id: 1,
-            author: "Julia",
-            quote: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-            img: "julia.webp"
-        }
-        ,
-        {
-            id: 2,
-            author: "André",
-            quote: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-            img: "andre.webp"
-        }
-        ,
-        {
-            id: 3,
-            author: "André",
-            quote: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-            img: "andre.webp"
-        }
-        ,
-    ]
-
-
     return (
         <section id='depoimentos' className='depoimentos'>
             <div className='container'>
@@ -51,7 +20,7 @@ const Depoimentos = () => {
                 <Swiper
                     className="depoimentos-wrapper"
                     slidesPerView={1}
-                    spaceBetween= {20}
+                    spaceBetween={20}
                     breakpoints={{
                         640: {
                             slidesPerView: 1
@@ -69,27 +38,66 @@ const Depoimentos = () => {
                     navigation={true}
                     scrollbar={{ draggable: true }}
                 >
-                    {
-                        quotes.map((item, index) => {
-                            return (
-                                <SwiperSlide key={index} className='single-depoimentos'>
-                                    <div className='quote-depoimento'>
-                                        <FaQuoteLeft />
-                                        <p>
-                                            {item.quote}
-                                        </p>
-                                        <FaQuoteRight />
-                                    </div>
-                                    <div className='author-depoimento'>
-                                        <Image alt={`Depoimento de ${item.author} à InterJud`} title={`Depoimento de ${item.author} à InterJud`} width={50} height={50} src={`/../public/assets/images/Depoimentos/${item.img}`} />
-                                        <p className='author-name'>
-                                            {item.author}
-                                        </p>
-                                    </div>
-                                </SwiperSlide>
-                            )
-                        })
-                    }
+                    <SwiperSlide className='single-depoimentos'>
+                        <div className='quote-depoimento'>
+                            <FaQuoteLeft />
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                            </p>
+                            <FaQuoteRight />
+                        </div>
+                        <div className='author-depoimento'>
+                            <Image alt={`Depoimento de Sandro à InterJud`} title={`Depoimento de Sandro à InterJud`} width={50} height={50} src={img} />
+                            <p className='author-name'>
+                                Sandro
+                            </p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='single-depoimentos'>
+                        <div className='quote-depoimento'>
+                            <FaQuoteLeft />
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                            </p>
+                            <FaQuoteRight />
+                        </div>
+                        <div className='author-depoimento'>
+                            <Image alt={`Depoimento de Sandro à InterJud`} title={`Depoimento de Sandro à InterJud`} width={50} height={50} src={img} />
+                            <p className='author-name'>
+                                Sandro
+                            </p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='single-depoimentos'>
+                        <div className='quote-depoimento'>
+                            <FaQuoteLeft />
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                            </p>
+                            <FaQuoteRight />
+                        </div>
+                        <div className='author-depoimento'>
+                            <Image alt={`Depoimento de Sandro à InterJud`} title={`Depoimento de Sandro à InterJud`} width={50} height={50} src={img} />
+                            <p className='author-name'>
+                                Sandro
+                            </p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className='single-depoimentos'>
+                        <div className='quote-depoimento'>
+                            <FaQuoteLeft />
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                            </p>
+                            <FaQuoteRight />
+                        </div>
+                        <div className='author-depoimento'>
+                            <Image alt={`Depoimento de Sandro à InterJud`} title={`Depoimento de Sandro à InterJud`} width={50} height={50} src={img} />
+                            <p className='author-name'>
+                                Sandro
+                            </p>
+                        </div>
+                    </SwiperSlide>
 
                 </Swiper>
 

7ded0fd7910ac07be75dde3d25d0b6581d41f965
Sandro <sandro.oliveira@rocky.ag>
2023-01-03 14:06:51 -0300

    feat: adding analytics to the project

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import { AuthContextProvider } from '../context/AuthContext'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 import ProtectedRoute from '../components/ProtectedRoute'
 import Router from 'next/router';
 
@@ -19,6 +20,21 @@ function MyApp({ Component, pageProps }) {
           <Component {...pageProps} />
         </ProtectedRoute>
       )}
+
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-P2W368DGW0" />
+
+      <Script id="google-analytics" strategy="afterInteractive" >
+        {
+          `
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+
+      gtag('config', 'G-P2W368DGW0');
+
+    `
+        }
+      </Script>
     </AuthContextProvider>
   )
 }

4f1203d9dd1fff6b0f96bfd5ce1cb25ff75271f6
Sandro Filho <sandrofilho98@yahoo.com.br>
2023-01-21 10:42:20 -0300

    fix: set typing lib right

--- components/Home/Hero/index.jsx
+++ components/Home/Hero/index.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
     useEffect(() => {
         AOS.init();
         const typed = new Typed(el.current, {
-            strings: [ "NEGOCIE SEU PROCESSO JUDICIAL", "GARANTA AGORA SEU DIREITO", "ANTECIPE SEUS SONHOS COM A INTERJUD", "RECEBA PROPOSTAS HOJE MESMO"], // Strings to display
+            strings: [ "NEGOCIE SEU PROCESSO JUDICIAL", "GARANTA AGORA SEUS DIREITOS", "ANTECIPE SEUS SONHOS COM A INTERJUD", "RECEBA PROPOSTAS HOJE MESMO"], // Strings to display
             // Speed settings, try diffrent values untill you get good results
 
             typeSpeed: 100,
